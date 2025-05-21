5e651ec030ffae46bc97b28bfd537b431574dc66
Rémi <remi.tan@etu.imt-nord.europe.fr>
2025-05-21 22:55:50 +0200

    Fix: nvim config

---
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -0,0 +1,183 @@
+return {
+  "saghen/blink.cmp",
+  -- optional: provides snippets for the snippet source
+  dependencies = { "rafamadriz/friendly-snippets", "L3MON4D3/LuaSnip" },
+
+  -- use a release tag to download pre-built binaries
+  version = "1.*",
+  -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+  -- build = 'cargo build --release',
+  -- If you use nix, you can build from source using latest nightly rust with:
+  -- build = 'nix run .#build-plugin',
+
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+    -- 'super-tab' for mappings similar to vscode (tab to accept)
+    -- 'enter' for enter to accept
+    -- 'none' for no mappings
+    --
+    -- All presets have the following mappings:
+    -- C-space: Open menu or open docs if already open
+    -- C-n/C-p or Up/Down: Select next/previous item
+    -- C-e: Hide menu
+    -- C-k: Toggle signature help (if signature.enabled = true)
+    -- Experimental signature help support
+    signature = {
+      enabled = true,
+      trigger = {
+        -- Show the signature help automatically
+        enabled = true,
+        -- Show the signature help window after typing any of alphanumerics, `-` or `_`
+        show_on_keyword = false,
+        blocked_trigger_characters = {},
+        blocked_retrigger_characters = {},
+        -- Show the signature help window after typing a trigger character
+        show_on_trigger_character = false,
+        -- Show the signature help window when entering insert mode
+        show_on_insert = true,
+        -- Show the signature help window when the cursor comes after a trigger character when entering insert mode
+        show_on_insert_on_trigger_character = false,
+      },
+      window = {
+        min_width = 1,
+        max_width = 100,
+        max_height = 10,
+        border = nil, -- Defaults to `vim.o.winborder` on nvim 0.11+ or 'padded' when not defined/<=0.10
+        winblend = 0,
+        winhighlight = "Normal:BlinkCmpSignatureHelp,FloatBorder:BlinkCmpSignatureHelpBorder",
+        scrollbar = false, -- Note that the gutter will be disabled when border ~= 'none'
+        -- Which directions to show the window,
+        -- falling back to the next direction when there's not enough space,
+        -- or another window is in the way
+        direction_priority = { "n", "s" },
+        -- Disable if you run into performance issues
+        treesitter_highlighting = true,
+        show_documentation = false,
+      },
+    },
+    --
+    -- See :h blink-cmp-config-keymap for defining your own keymap
+    -- channels
+    keymap = {
+      preset = "default",
+      ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+      ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+      ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+      ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+      ["<Tab>"] = { "accept", "fallback" },
+      ["<CR>"] = { "accept", "fallback" },
+    },
+
+    appearance = {
+      -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+      -- Adjusts spacing to ensure icons are aligned
+      nerd_font_variant = "mono",
+    },
+
+    -- (Default) Only show the documentation popup when manually triggered
+    completion = {
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 500,
+      },
+      ghost_text = { enabled = vim.g.ai_cmp },
+      list = {
+        selection = { preselect = false, auto_insert = false },
+      },
+      menu = {
+        scrollbar = true,
+        auto_show = true,
+        draw = {
+          columns = {
+            { "kind_icon" },
+            { "label", "label_description", gap = 1 },
+            { "kind", gap = 1 },
+            { "label_description", gap = 1 },
+            { "source_name", gap = 1 },
+          },
+          components = {
+            kind_icon = {
+              ellipsis = false,
+              width = { fill = true },
+              text = function(ctx)
+                local kind_icons = {
+                  Function = "λ", -- Lambda symbol for functions
+                  Method = "∂", -- Lambda symbol for methods
+                  Field = "󰀫", -- Lambda symbol for methods
+                  Variable = "󰀫", -- Lambda symbol for methods
+                  Property = "󰀫", -- Lambda symbol for methods
+                  Keyword = "k", -- Lambda symbol for methods
+                  Struct = "Π", -- Lambda symbol for methods
+                  Enum = "τ", -- Lambda symbol for methods
+                  EnumMember = "τ", -- Lambda symbol for methods
+                  Snippet = "⊂",
+                  Text = "τ",
+                  Module = "⌠",
+                  Constructor = "∑",
+                }
+
+                local icon = kind_icons[ctx.kind]
+                if icon == nil then
+                  icon = ctx.kind_icon
+                end
+                return icon
+              end,
+            },
+          },
+        },
+      },
+    },
+
+    -- Default list of enabled providers defined so that you can extend it
+    -- elsewhere in your config, without redefining it, due to `opts_extend`
+    sources = {
+      default = { "lsp", "path", "snippets", "buffer" },
+    },
+
+    -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+    -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+    -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+    --
+    -- See the fuzzy documentation for more information
+    fuzzy = { implementation = "prefer_rust_with_warning" },
+
+    cmdline = {
+      enabled = true,
+      -- use 'inherit' to inherit mappings from top level `keymap` config
+      keymap = { preset = "cmdline" },
+      sources = function()
+        local type = vim.fn.getcmdtype()
+        -- Search forward and backward
+        if type == "/" or type == "?" then
+          return { "buffer" }
+        end
+        -- Commands
+        if type == ":" or type == "@" then
+          return { "cmdline" }
+        end
+        return {}
+      end,
+      completion = {
+        trigger = {
+          show_on_blocked_trigger_characters = {},
+          show_on_x_blocked_trigger_characters = {},
+        },
+        list = {
+          selection = {
+            -- When `true`, will automatically select the first item in the completion list
+            preselect = true,
+            -- When `true`, inserts the completion item automatically when selecting it
+            auto_insert = true,
+          },
+        },
+        -- Whether to automatically show the window when new completion items are available
+        menu = { auto_show = true },
+        -- Displays a preview of the selected item on the current line
+        ghost_text = { enabled = true },
+      },
+    },
+  },
+  opts_extend = { "sources.default" },
+}
